0a227781bfc6bcac07000196c7cda5d1d529a0bd
willy <marti1125@gmail.com>
2019-03-21 16:18:29 -0300

    add example of test

---
+++ doer/calculate_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculate(t *testing.T) {
+	/*if Calculate(2) != 4 {
+		t.Error("Expected 2 + 2 to equal 4")
+	}*/
+	assert.Equal(t, Calculate(2), 4)
+}
+
+func TestStatusNotDown(t *testing.T) {
+	status := "fake"
+	assert.NotEqual(t, status, "down")
+}
+
+func TestCalculate2(t *testing.T) {
+	assert := assert.New(t)
+
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{-5, -3},
+		{99999, 100001},
+	}
+
+	for _, test := range tests {
+		assert.Equal(Calculate(test.input), test.expected)
+	}
+
+}
---
+++ doer/main.go
@@ -0,0 +1,11 @@
+package main
+
+//import "fmt"
+
+func Calculate(x int) int {
+	return x + 2
+}
+
+/*func es() {
+	fmt.Println("Tests....")
+}*/
